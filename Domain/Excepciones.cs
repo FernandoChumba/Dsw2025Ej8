26b9e7fed892b91d738222d0b107cb9b83b8ad12
carolina <carounavaro@gmail.com>
2025-05-02 20:19:35 -0300

    Agregando Excepciones MontoNoValido CuentaNoActiva y SaldoInssuficiente

---
+++ Domain/Excepciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain
+{
+    public class Excepciones
+    {
+        public class MontoNoValido : Exception
+        {
+            public MontoNoValido() : base("El monto recibido no es válido (debe ser mayor a cero).") { }
+        }
+
+        public class CuentaNoActiva : Exception
+        {
+            public CuentaNoActiva(string estado)
+                : base($"No se puede operar con la cuenta {estado}.") { }
+        }
+        public class SaldoInsuficiente : Exception
+        {
+            public SaldoInsuficiente() : base("La cuenta no cuenta con saldo para la operación solicitada. Fue suspendida.") { }
+        }
+
+
+    }
+}
